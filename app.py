38dea05b6b065f3718979362d2dbceb8b5baff6a
Lakshmi Prasanth Devarapalli <116416695+PrashanthDev31@users.noreply.github.com>
2026-03-31 10:34:37 -0400

    added files and folders

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker app!"
-@app.route("/health")
-def health():
-    return {"status": "ok"}
+    return "Docker image build is working!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
